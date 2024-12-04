970e000a83651d03eb2eafcd517278b382b4823e
Joao Martins <willful-upriver.0j@icloud.com>
2024-12-04 17:50:24 +0200

    chore: install deps and tidy up

--- ui/src/vite-env.d.ts
+++ ui/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URI: string;
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_POLY_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
